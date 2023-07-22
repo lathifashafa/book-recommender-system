95532c603e58e9c4557d7429ea719e4f468338fb
lathifashafa <104220839+lathifashafa@users.noreply.github.com>
2023-07-22 16:13:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown(
         /* Gambar background untuk desktop */
         @media (min-width: 768px) {
             .stApp {
-                background-image: url("https://i.imgur.com/EgQwENZ.png");
+                background-image: url("https://i.imgur.com/WgfptGM.png");
                 background-size: 100% 100%;
                 background-repeat: no-repeat;
             }
